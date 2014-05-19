553b42b15c51752e0eaae17d722b03f4dc4cc199
Sonny Nguyen <phplaw@gmail.com>
2014-05-20 02:15:28 +0700

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,18 @@
+var ships = new Backbone.Collection;
+
+ships.on("add", function(ship) {
+  alert("Ahoy " + ship.get("name") + "!");
+});
+
+ships.add([
+  {name: "Flying Dutchman"},
+  {name: "Black Pearl"}
+]);
+
+
+
+initialize: function () {
+    this.collection = new ProjectsCollection();
+    this.collection.bind("reset", _.bind(this.render, this));
+    this.collection.fetch(...);
+}
